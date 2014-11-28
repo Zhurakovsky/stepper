c05035c94aa7b92a21696fbd27818a198d1af3dd
Zhurakovsky <yuri.zhurakovsky@gmail.com>
2014-11-28 13:02:32 +0000

    2014-11-28

--- README.md
+++ README.md
@@ -2,3 +2,78 @@ stepper
 =======
 
 how to control stepper motor from raspberry pi and bcm2835
+
+so, there are effective sequense of instructions for stepper motor:
+8-steps
+    while ( counter > 0 ) {
+        bcm2835_gpio_write(PIN1, HIGH);
+        bcm2835_gpio_write(PIN2, LOW);
+        bcm2835_gpio_write(PIN3, LOW);
+        bcm2835_gpio_write(PIN4, HIGH);
+        bcm2835_delay(50);
+        bcm2835_gpio_write(PIN1, LOW);
+        bcm2835_gpio_write(PIN2, LOW);
+        bcm2835_gpio_write(PIN3, LOW);
+        bcm2835_gpio_write(PIN4, HIGH);
+        bcm2835_delay(50);
+        bcm2835_gpio_write(PIN1, LOW);
+        bcm2835_gpio_write(PIN2, HIGH);
+        bcm2835_gpio_write(PIN3, LOW);
+        bcm2835_gpio_write(PIN4, HIGH);
+        bcm2835_delay(50);
+        bcm2835_gpio_write(PIN1, LOW);
+        bcm2835_gpio_write(PIN2, HIGH);
+        bcm2835_gpio_write(PIN3, LOW);
+        bcm2835_gpio_write(PIN4, LOW);
+        bcm2835_delay(50);
+        bcm2835_gpio_write(PIN1, LOW);
+        bcm2835_gpio_write(PIN2, HIGH);
+        bcm2835_gpio_write(PIN3, HIGH);
+        bcm2835_gpio_write(PIN4, LOW);
+        bcm2835_delay(50);
+        bcm2835_gpio_write(PIN1, LOW);
+        bcm2835_gpio_write(PIN2, LOW);
+        bcm2835_gpio_write(PIN3, HIGH);
+        bcm2835_gpio_write(PIN4, LOW);
+        bcm2835_delay(50);
+        bcm2835_gpio_write(PIN1, HIGH);
+        bcm2835_gpio_write(PIN2, LOW);
+        bcm2835_gpio_write(PIN3, HIGH);
+        bcm2835_gpio_write(PIN4, LOW);
+        bcm2835_delay(50);
+        bcm2835_gpio_write(PIN1, HIGH);
+        bcm2835_gpio_write(PIN2, LOW);
+        bcm2835_gpio_write(PIN3, LOW);
+        bcm2835_gpio_write(PIN4, LOW);
+        bcm2835_delay(50);
+        counter -= 1;
+    }
+    
+4-steps:
+    while ( counter > 0 ) {
+        bcm2835_gpio_write(PIN1, HIGH);
+        bcm2835_gpio_write(PIN2, LOW);
+        bcm2835_gpio_write(PIN3, LOW);
+        bcm2835_gpio_write(PIN4, HIGH);
+        bcm2835_delay(50);
+
+        bcm2835_gpio_write(PIN1, LOW);
+        bcm2835_gpio_write(PIN2, HIGH);
+        bcm2835_gpio_write(PIN3, LOW);
+        bcm2835_gpio_write(PIN4, HIGH);
+        bcm2835_delay(50);
+
+        bcm2835_gpio_write(PIN1, LOW);
+        bcm2835_gpio_write(PIN2, HIGH);
+        bcm2835_gpio_write(PIN3, HIGH);
+        bcm2835_gpio_write(PIN4, LOW);
+        bcm2835_delay(50);
+
+        bcm2835_gpio_write(PIN1, HIGH);
+        bcm2835_gpio_write(PIN2, LOW);
+        bcm2835_gpio_write(PIN3, HIGH);
+        bcm2835_gpio_write(PIN4, LOW);
+        bcm2835_delay(50);
+
+        counter -= 1;
+    }
--- gpio_motor.c
+++ gpio_motor.c
@@ -7,65 +7,105 @@
 #define PIN3 RPI_V2_GPIO_P1_18
 #define PIN4 RPI_V2_GPIO_P1_22
 
+void prepareStep(char *s[], int steps, int counter);
+void makeStep(char *ws);
+
+
 int main() {
+    int scale = 0;
+    int i;
+    int counter =  -1;
+    char* steps4[4];
+    char* steps8[8];
+    char* workingSteps;
+    
     if ( !bcm2835_init() ) {
         return 1;
     }
-    int counter =  -1;
-    printf("Input number of steps: ");
-    scanf("%d", &counter);
-    bcm2835_gpio_fsel(PIN1, BCM2835_GPIO_FSEL_OUTP);
-    bcm2835_gpio_fsel(PIN2, BCM2835_GPIO_FSEL_OUTP);
-    bcm2835_gpio_fsel(PIN3, BCM2835_GPIO_FSEL_OUTP);
-    bcm2835_gpio_fsel(PIN4, BCM2835_GPIO_FSEL_OUTP);
-    while ( counter > 0 ) {
-        bcm2835_gpio_write(PIN1, HIGH);
-        bcm2835_gpio_write(PIN2, LOW);
-        bcm2835_gpio_write(PIN3, LOW);
-        bcm2835_gpio_write(PIN4, HIGH);
-        bcm2835_delay(50);
-        bcm2835_gpio_write(PIN1, LOW);
-        bcm2835_gpio_write(PIN2, LOW);
-        bcm2835_gpio_write(PIN3, LOW);
-        bcm2835_gpio_write(PIN4, HIGH);
-        bcm2835_delay(50);
-        bcm2835_gpio_write(PIN1, LOW);
-        bcm2835_gpio_write(PIN2, HIGH);
-        bcm2835_gpio_write(PIN3, LOW);
-        bcm2835_gpio_write(PIN4, HIGH);
-        bcm2835_delay(50);
-        bcm2835_gpio_write(PIN1, LOW);
-        bcm2835_gpio_write(PIN2, HIGH);
-        bcm2835_gpio_write(PIN3, LOW);
-        bcm2835_gpio_write(PIN4, LOW);
-        bcm2835_delay(50);
-        bcm2835_gpio_write(PIN1, LOW);
-        bcm2835_gpio_write(PIN2, HIGH);
-        bcm2835_gpio_write(PIN3, HIGH);
-        bcm2835_gpio_write(PIN4, LOW);
-        bcm2835_delay(50);
-        bcm2835_gpio_write(PIN1, LOW);
-        bcm2835_gpio_write(PIN2, LOW);
-        bcm2835_gpio_write(PIN3, HIGH);
-        bcm2835_gpio_write(PIN4, LOW);
-        bcm2835_delay(50);
-        bcm2835_gpio_write(PIN1, HIGH);
-        bcm2835_gpio_write(PIN2, LOW);
-        bcm2835_gpio_write(PIN3, HIGH);
-        bcm2835_gpio_write(PIN4, LOW);
-        bcm2835_delay(50);
-        bcm2835_gpio_write(PIN1, HIGH);
-        bcm2835_gpio_write(PIN2, LOW);
-        bcm2835_gpio_write(PIN3, LOW);
-        bcm2835_gpio_write(PIN4, LOW);
-        bcm2835_delay(50);
-        counter -= 1;
+    
+    steps4[0] = "1 0 0 1";
+    steps4[1] = "0 1 0 1";
+    steps4[2] = "0 1 1 0";
+    steps4[3] = "1 0 1 0";
+    
+    steps8[0] = "1 0 0 1";
+    steps8[1] = "0 0 0 1";
+    steps8[2] = "0 1 0 1";
+    steps8[3] = "0 1 0 0";
+    steps8[4] = "0 1 1 0";
+    steps8[5] = "0 0 1 0";
+    steps8[6] = "1 0 1 0";
+    steps8[7] = "1 0 0 0";
+    
+    printf("Input 4 or 8 steps per cycle '0' for exit: ");
+    scanf("%d", &scale);
+    while ( scale != 4 && scale != 8 ) {
+        if ( scale == 0 ) {
+            break;
+        }
+        printf("Input number of steps per cycle '0' for exit: ");
+        scanf("%d", &scale);
     }
+    
+    if ( scale != 0 ) {
+        printf("Input number of steps: ");
+        scanf("%d", &counter);
+    }
+    
+    if ( scale == 4 ) {
+        prepareStep(steps4, scale, counter);
+    } else if ( scale == 8 ) {
+        prepareStep(steps8, scale, counter);
+    } else {
+        return 0;
+    }
+    
+
     bcm2835_gpio_write(PIN1, LOW);
     bcm2835_gpio_write(PIN2, LOW);
     bcm2835_gpio_write(PIN3, LOW);
     bcm2835_gpio_write(PIN4, LOW);
     bcm2835_close();
     return 0;
+}
+
+void prepareStep(char *s[], int scale, int counter) {
+    int a[4];
+    int i;
+    char *ws;
+    char *blankString = "0 0 0 0";
+    
+    bcm2835_gpio_fsel(PIN1, BCM2835_GPIO_FSEL_OUTP);
+    bcm2835_gpio_fsel(PIN2, BCM2835_GPIO_FSEL_OUTP);
+    bcm2835_gpio_fsel(PIN3, BCM2835_GPIO_FSEL_OUTP);
+    bcm2835_gpio_fsel(PIN4, BCM2835_GPIO_FSEL_OUTP);
+    
+    while ( counter > 0 ) {
+        for ( i = 0; i < scale; i++ ) {
+            ws = s[i];
+            makeStep(ws);
+        }
+    }
+    
+}
+
+void makeStep(char *ws) {
+    int a[4];
+    int i;
+    for ( i = 0; i < 4 && *ws != '\0'; ) {
+        if ( *ws != ' ' ) {
+            a[i] = *ws - '0';
+            i++;
+            ws++;
+        } else {
+            ws++;
+        }
+    }
+    
+    bcm2835_gpio_write(PIN1, a[0]);
+    bcm2835_gpio_write(PIN2, a[1]);
+    bcm2835_gpio_write(PIN3, a[2]);
+    bcm2835_gpio_write(PIN4, a[3]);
+    bcm2835_delay(50);
 }
 
