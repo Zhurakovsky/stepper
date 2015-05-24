1e546d10f99a0a06cbd44824123d1ddfcb7aacc9
Zhurakovsky <yuri.zhurakovsky@gmail.com>
2015-05-24 11:17:22 +0000

    20150524 mouse usage try

--- gpio_motor.c
+++ gpio_motor.c
@@ -1,20 +1,30 @@
-// compile: sudo gcc gpiotest.c -o gpiotest.exe -l bcm2835
+ // compile: sudo gcc gpiotest.c -o gpiotest.exe -l bcm2835
 // version 0.1 master
 #include <bcm2835.h>
 #include <stdio.h>
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <linux/input.h>
+#include <fcntl.h> 
+#define MOUSEFILE "/dev/input/event1"
 
 #define PIN1 RPI_V2_GPIO_P1_12
 #define PIN2 RPI_V2_GPIO_P1_16
 #define PIN3 RPI_V2_GPIO_P1_18
 #define PIN4 RPI_V2_GPIO_P1_22
+/*
 #define PIN5 RPI_V2_GPIO_P1_07
 #define PIN6 RPI_V2_GPIO_P1_11
 #define PIN7 RPI_V2_GPIO_P1_15
-
+*/
 void prepareStep(char *s[], int steps, int counter);
 void makeStep(char *ws);
 
 int main() {
+    int fd;
+    struct input_event ie;
+    
     int scale = 0;
     int i;
     int counter =  -1;
@@ -26,7 +36,16 @@ int main() {
     if ( !bcm2835_init() ) {
         return 1;
     }
-    
+    // открываем файл мышки для отслеживания событий
+    if((fd = open(MOUSEFILE, O_RDONLY)) == -1) {
+        perror("opening device");
+        exit(EXIT_FAILURE);
+    }
+    while(read(fd, &ie, sizeof(struct input_event))) {
+        printf("time %ld.%06ld\ttype %d\tcode %d\tvalue %d\n",
+               ie.time.tv_sec, ie.time.tv_usec, ie.type, ie.code, ie.value);
+    }
+    /*
     steps4[0] = "1 0 0 1";
     steps4[1] = "0 1 0 1";
     steps4[2] = "0 1 1 0";
@@ -65,6 +84,7 @@ int main() {
     }
     
     makeStep(blankString);
+    */
     bcm2835_close();
     
     return 0;
@@ -75,44 +95,46 @@ void prepareStep(char *s[], int scale, int counter) {
     int i;
     char *ws;
     char *blankString = "0 0 0 0";
-    uint8_t valueOnSix, valueOnSeven;
-    char flagButtonRelease = 1;
+    //uint8_t valueOnSix, valueOnSeven;
+    //char flagButtonRelease = 1;
     
     bcm2835_gpio_fsel(PIN1, BCM2835_GPIO_FSEL_OUTP);
     bcm2835_gpio_fsel(PIN2, BCM2835_GPIO_FSEL_OUTP);
     bcm2835_gpio_fsel(PIN3, BCM2835_GPIO_FSEL_OUTP);
     bcm2835_gpio_fsel(PIN4, BCM2835_GPIO_FSEL_OUTP);
+    /*
+    попробуем без кнопок. управляем мышкой.
     bcm2835_gpio_fsel(PIN5, BCM2835_GPIO_FSEL_OUTP);
     bcm2835_gpio_fsel(PIN6, BCM2835_GPIO_FSEL_INPT);
     bcm2835_gpio_fsel(PIN7, BCM2835_GPIO_FSEL_INPT);
     bcm2835_gpio_write(PIN5, HIGH);
     bcm2835_gpio_write(PIN6, LOW);
     bcm2835_gpio_write(PIN7, LOW);
-    
+    */
     while ( 1 ) {
-        valueOnSix = bcm2835_gpio_lev(PIN6);
-        valueOnSeven = bcm2835_gpio_lev(PIN7);
-        if ( valueOnSix == 1 && flagButtonRelease == 1 ) {
+    //    valueOnSix = bcm2835_gpio_lev(PIN6);
+    //    valueOnSeven = bcm2835_gpio_lev(PIN7);
+    //    if ( valueOnSix == 1 && flagButtonRelease == 1 ) {
             for ( i = 0; i < scale; i++ ) {
                 ws = s[i];
                 makeStep(ws);
             }
-            flagButtonRelease = 0;
+    //        flagButtonRelease = 0;
 
-        } else if ( valueOnSeven == 1  && flagButtonRelease == 1 ) {
-            for ( i = scale - 1; i >= 0; i-- ) {
-                ws = s[i];
-                makeStep(ws);
-            }
-            flagButtonRelease = 0;
+    //    } else if ( valueOnSeven == 1  && flagButtonRelease == 1 ) {
+     //       for ( i = scale - 1; i >= 0; i-- ) {
+      //          ws = s[i];
+      //          makeStep(ws);
+     //       }
+     //       flagButtonRelease = 0;
 
-        } else {
-            if ( !valueOnSix && !valueOnSeven ) {
-                flagButtonRelease = 1;
-            }
-        }
-        valueOnSix = 0;
-        valueOnSeven = 0;
+     //   } else {
+     //       if ( !valueOnSix && !valueOnSeven ) {
+     //           flagButtonRelease = 1;
+      //      }
+      //  }
+      //  valueOnSix = 0;
+      //  valueOnSeven = 0;
         makeStep(blankString);
     }
 }
---
+++ mouse.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <linux/input.h>
+#include <fcntl.h> 
+
+#define MOUSEFILE "/dev/input/event1"
+
+int main() {
+    int fd;
+    struct input_event ie;
+    
+    if((fd = open(MOUSEFILE, O_RDONLY)) == -1) {
+        perror("opening device");
+        exit(EXIT_FAILURE);
+    }
+    while(read(fd, &ie, sizeof(struct input_event))) {
+        printf("time %ld.%06ld\ttype %d\tcode %d\tvalue %d\n",
+               ie.time.tv_sec, ie.time.tv_usec, ie.type, ie.code, ie.value);
+    }
+    return 0;
+}
+
+/*
+ * The event structure itself
+ 
+struct input_event {
+    struct timeval time;
+    __u16 type;
+    __u16 code;
+    __s32 value;
+};
+
+/*
+ * Event types
+ 
+#define EV_SYN 0x00 
+#define EV_KEY 0x01 
+#define EV_REL 0x02 
+#define EV_ABS 0x03 
+#define EV_MSC 0x04 
+#define EV_SW 0x05 
+#define EV_LED 0x11 
+#define EV_SND 0x12 
+#define EV_REP 0x14 
+#define EV_FF 0x15 
+#define EV_PWR 0x16 
+#define EV_FF_STATUS 0x17 
+#define EV_MAX 0x1f 
+#define EV_CNT (EV_MAX+1) 
+#define BTN_MOUSE 0x110 //272 
+#define BTN_LEFT 0x110 //272 
+#define BTN_RIGHT 0x111 //273 
+#define BTN_MIDDLE 0x112 //274 
+#define BTN_SIDE 0x113 
+#define BTN_EXTRA 0x114 
+#define BTN_FORWARD 0x115 
+#define BTN_BACK 0x116 
+#define BTN_TASK 0x117
+*/
